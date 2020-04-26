5371cbbd4a1c16e80d6687c28bed6c07d2e49c6c
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2020-04-25 23:55:39 -0700

    docs for App component

--- src/app/App.js
+++ src/app/App.js
@@ -5,16 +5,30 @@ import WardrobePage from "./WardrobePage";
 
 import ApolloClient from "apollo-boost";
 
+/**
+ * client is the global Apollo Client instance we use for GraphQL queries. This
+ * is how we communicate with the server!
+ */
 const client = new ApolloClient({
   uri: "/api/graphql",
   cacheRedirects: {
     Query: {
+      // Teach Apollo how to serve `items` queries from the cache. That way,
+      // when you remove an item from your outfit, or add an item from search,
+      // Apollo knows it already has the data it needs and doesn't need to ask
+      // the server again!
       items: (_, args, { getCacheKey }) =>
         args.ids.map((id) => getCacheKey({ __typename: "Item", id })),
     },
   },
 });
 
+/**
+ * App is the entry point of our application. There's not a ton of exciting
+ * stuff happening here, mostly just setting up some globals and theming!
+ *
+ * To really dive into the code, try going down into a page component!
+ */
 function App() {
   return (
     <ApolloProvider client={client}>
