a164235c0e2377b07531814c0a94f82901c71008
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2020-05-03 02:13:46 -0700

    add cache-control to validPetPoses API

--- api/validPetPoses.js
+++ api/validPetPoses.js
@@ -2,5 +2,10 @@ import getValidPetPoses from "../src/server/getValidPetPoses";
 
 export default async (req, res) => {
   const buffer = await getValidPetPoses();
+
+  // Cache for 1 hour. This will also cache at Vercel's CDN, so the function
+  // shouldn't even get run very often at all!
+  res.setHeader("Cache-Control", "max-age=3600");
+
   res.status(200).send(buffer);
 };
