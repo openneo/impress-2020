785d119324e1fc076d0267ce453c1845f000c7d3
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2020-05-10 01:01:14 -0700

    set explicit page title on homepage

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -10,6 +10,7 @@ import {
   useTheme,
   useToast,
 } from "@chakra-ui/core";
+import { Helmet } from "react-helmet";
 import { useHistory } from "react-router-dom";
 import { useLazyQuery } from "@apollo/react-hooks";
 
@@ -20,33 +21,38 @@ import SpeciesColorPicker from "./SpeciesColorPicker";
 
 function HomePage() {
   return (
-    <Flex
-      color="green.800"
-      direction="column"
-      p="6"
-      align="center"
-      textAlign="center"
-    >
-      <Box height="8" />
-      <Box
-        as="img"
-        src={HomepageSplashImg}
-        width="200px"
-        height="200px"
-        rounded="lg"
-        boxShadow="md"
-      />
-      <Box height="4" />
-      <Heading1>Dress to Impress</Heading1>
-      <Box height="8" />
-      <StartOutfitForm />
-      <Box height="4" />
-      <Box fontStyle="italic" fontSize="sm">
-        or
-      </Box>
-      <Box height="4" />
-      <SubmitPetForm />
-    </Flex>
+    <>
+      <Helmet>
+        <title>Dress to Impress</title>
+      </Helmet>
+      <Flex
+        color="green.800"
+        direction="column"
+        p="6"
+        align="center"
+        textAlign="center"
+      >
+        <Box height="8" />
+        <Box
+          as="img"
+          src={HomepageSplashImg}
+          width="200px"
+          height="200px"
+          rounded="lg"
+          boxShadow="md"
+        />
+        <Box height="4" />
+        <Heading1>Dress to Impress</Heading1>
+        <Box height="8" />
+        <StartOutfitForm />
+        <Box height="4" />
+        <Box fontStyle="italic" fontSize="sm">
+          or
+        </Box>
+        <Box height="4" />
+        <SubmitPetForm />
+      </Flex>
+    </>
   );
 }
 
