93760db1e007342ac807f52222b781cfc15b456d
Matchu <matchu1993@gmail.com>
2020-07-31 15:30:25 -0700

    add support permissions to setup SQL

--- setup-mysql-user.sql
+++ setup-mysql-user.sql
@@ -1,4 +1,4 @@
--- Public data tables
+-- Public data tables: read
 GRANT SELECT ON openneo_impress.colors TO impress2020;
 GRANT SELECT ON openneo_impress.color_translations TO impress2020;
 GRANT SELECT ON openneo_impress.items TO impress2020;
@@ -11,6 +11,10 @@ GRANT SELECT ON openneo_impress.species_translations TO impress2020;
 GRANT SELECT ON openneo_impress.swf_assets TO impress2020;
 GRANT SELECT ON openneo_impress.zones TO impress2020;
 GRANT SELECT ON openneo_impress.zone_translations TO impress2020;
+
+-- Public data tables: write
+GRANT UPDATE ON openneo_impress.items TO impress2020;
+GRANT UPDATE ON openneo_impress.swf_assets TO impress2020;
 
 -- User data tables
 GRANT SELECT ON openneo_impress.item_outfit_relationships TO impress2020;
