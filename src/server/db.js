df2d814c13bb7e190f07706e83c9ac88ad085399
Matchu <matchu1993@gmail.com>
2020-10-06 06:11:22 -0700

    enable running against a local dev database
    had to add some missing tables, but it seems to work! (some known errors though, from assumptions we make e.g. blue acaras existing)

--- src/server/db.js
+++ src/server/db.js
@@ -2,11 +2,33 @@ const mysql = require("mysql2");
 
 let globalDbs = new Map();
 
+// We usually run against the production database, even in local testing,
+// to easily test against real data. (Not a wise general practice, but fine
+// for this low-stakes project and small dev team with mostly read-only
+// operations!)
+//
+// But you can also specify `DB_ENV=development` to use a local database,
+// which is especially helpful for end-to-end modeling testing.
+const defaultOptions =
+  process.env["DB_ENV"] === "development"
+    ? {
+        host: "localhost",
+        user: "impress_2020_dev",
+        password: "impress_2020_dev",
+        database: "impress_2020_dev",
+      }
+    : {
+        host: "impress.openneo.net",
+        user: process.env["IMPRESS_MYSQL_USER"],
+        password: process.env["IMPRESS_MYSQL_PASSWORD"],
+        database: "openneo_impress",
+      };
+
 async function connectToDb({
-  host = "impress.openneo.net",
-  user = process.env["IMPRESS_MYSQL_USER"],
-  password = process.env["IMPRESS_MYSQL_PASSWORD"],
-  database = "openneo_impress",
+  host = defaultOptions.host,
+  user = defaultOptions.user,
+  password = defaultOptions.password,
+  database = defaultOptions.database,
 } = {}) {
   if (globalDbs.has(host)) {
     return globalDbs.get(host);
