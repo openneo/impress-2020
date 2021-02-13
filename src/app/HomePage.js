e0e85b2add7f171ece28b97eae6483931aacd60c
Matchu <matchu1993@gmail.com>
2021-02-12 16:11:30 -0800

    Mention zone info on home page updates section

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -515,10 +515,11 @@ function FeedbackFormPitch() {
           flex={{ base: "0 0 auto", sm: "0 1 50%" }}
         >
           <Box as="h3" fontWeight="600">
-            New updates (Feb 9)
+            New updates (Feb 12)
           </Box>
           <UnorderedList>
-            <ListItem>HTML5 info on item pages</ListItem>
+            <ListItem>Zone info on item page!</ListItem>
+            <ListItem>HTML5 status for items!</ListItem>
             <ListItem>Better item page previews</ListItem>
           </UnorderedList>
         </Box>
