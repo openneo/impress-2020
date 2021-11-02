52526c09e8e5fb8c3a25670f2d060da152a0e333
Matchu <matchu1993@gmail.com>
2021-11-01 22:33:23 -0700

    [WIP] Move redirects to Next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@ module.exports = {
   env: {
     PUBLIC_URL: "",
   },
+  async redirects() {
+    return [
+      {
+        source: "/user/:userId/closet",
+        destination: "/user/:userId/lists",
+        permanent: true,
+      },
+      {
+        source: "/user/:userId/items",
+        destination: "/user/:userId/lists",
+        permanent: true,
+      },
+    ];
+  },
 };
