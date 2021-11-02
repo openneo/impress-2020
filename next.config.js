5b0919f23bd4b1b6a5f5acd004e3e91ff7f768fa
Matchu <matchu1993@gmail.com>
2021-11-01 22:42:23 -0700

    [WIP] Use nice outfit image paths

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ module.exports = {
   env: {
     PUBLIC_URL: "",
   },
+  async rewrites() {
+    return [
+      {
+        source: "/outfits/:id/:size(150|300|600).png",
+        destination: "/api/outfitImage?size=:size&id=:id",
+      },
+      {
+        source: "/outfits/:id/v/:updatedAt/:size(150|300|600).png",
+        destination: "/api/outfitImage?size=:size&id=:id&updatedAt=:updatedAt",
+      },
+    ];
+  },
   async redirects() {
     return [
       {
