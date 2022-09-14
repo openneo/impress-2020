4c1bda274aff91bc0ed7ef3219d4c1cc8550fa8e
Matchu <matchu1993@gmail.com>
2022-09-13 21:07:36 -0700

    Convert openneo_id.users to InnoDB
    I'm working on account creation, and got tripped up by transactions not working correctly, because MyISAM tables simply don't support them! Took too long to debug this lol :p
    
    Anyway, I made the change in the prod db, and then re-downloaded the schema files to here.

--- scripts/setup-mysql-dev-schema-id.sql
+++ scripts/setup-mysql-dev-schema-id.sql
@@ -44,7 +44,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `index_users_on_email` (`email`),
   UNIQUE KEY `index_users_on_reset_password_token` (`reset_password_token`),
   UNIQUE KEY `index_users_on_unlock_token` (`unlock_token`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +56,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-13 20:17:22
+-- Dump completed on 2022-09-13 21:04:40
